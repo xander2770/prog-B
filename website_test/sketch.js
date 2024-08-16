db18fe08ac6427015d3401820091673508ee3ff7
xander2770 <alexander.mads.laursen@gmail.com>
2024-08-16 11:13:21 +0200

    me has made page that move

---
+++ website_test/sketch.js
@@ -0,0 +1,29 @@
+
+let currentpage = 1
+
+let pages // en arry med alle sider
+
+function setup(){
+  pages = selectAll('.page')
+  //console.log(pages.length)
+
+}
+function shiftpage(num){
+  if(num == "ArrowLeft"){
+    num = currentpage-1
+  }
+  if(num == "ArrowRight"){
+    num = currentpage+1
+  }
+  if(isNaN(num) || num > pages.length || num == 0){
+  alert("er du lidt dum, du kan kun klikke tal som har en side")
+    return
+  }
+  select("#page" + currentpage).removeClass("visible")
+  currentpage = num
+  select("#page" + currentpage).addClass("visible")
+}
+
+function keyPressed(){
+  shiftpage(key)
+}
