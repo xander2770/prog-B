1905a8fe27bc1ca1cda18ed3feceb68e90303982
xander2770 <alexander.mads.laursen@gmail.com>
2024-08-30 16:44:24 +0200

    sss

--- website_css_trics copy/sketch.js
+++ website_css_trics copy/sketch.js
@@ -1,40 +1,68 @@
+let currentpage = 1;
+let menuitems; // array with all menu items
+let pages; // array with all pages
+let menubutton; // single menu button
 
-let currentpage = 1
-let menuitems //arry med alle menuer
-let pages // en arry med alle sider
+function setup() {
+  pages = selectAll('.page');
+  menuitems = selectAll('.menuitem');
+  menubutton = select('.menubutton'); // select the single menubutton element
 
-function setup(){
-
-  pages = selectAll('.page')
-  menuitems = selectAll('.menuitem')
-  //console.log(pages.length)
-
-  for( m of menuitems){
-    m.mousePressed( function(e){
-      let nr = e.target.id.slice(-1)
-      shiftpage(nr)
-    })
+  // Attach mousePressed event to each menuitem
+  for (let m of menuitems) {
+    m.mousePressed(function(e) {
+      let nr = e.target.id.slice(-1);
+      shiftpage(nr);
+    });
   }
+  
+  select('header #menu4').mouseOver(function(){
+    select('header').style('border-bottom', '6px solid black')
+  })
+  select('header #menu4').mouseOut(function(){
+    select('header').style('border-bottom', '0px solid black')
+  })
+  // Attach mousePressed event to the single menubutton
+  menubutton.mousePressed(function() {
+    select('header').removeClass("hidden");
+  });
 
-  shiftpage(currentpage)
+  let header = select('header');
+
+  header.mouseOver(() => {
+    header.removeClass("hidden");
+  });
+
+  header.mouseOut(() => {
+    header.addClass("hidden");
+  });
+
+  shiftpage(currentpage);
 }
-function shiftpage(num){
-  if(num == "ArrowLeft"){
-    num = currentpage-1
+
+function draw(){
+  let menu4 = select('header #menu4')
+  if (menu4.classList.contains('active')) {
+    select('header').style('border-bottom', '6px solid red')
   }
-  if(num == "ArrowRight"){
-    num = currentpage+1
+}
+function shiftpage(num) {
+  if (num == "ArrowLeft") {
+    num = currentpage - 1;
   }
-  if(isNaN(num) || num > pages.length || num == 0){
-    return
+  if (num == "ArrowRight") {
+    num = currentpage + 1;
   }
-  select("#page" + currentpage).removeClass("visible")
-  select("#menu" + currentpage).removeClass("active")
-  currentpage = num
-  select("#page" + currentpage).addClass("visible")
-  select("#menu" + currentpage).addClass("active")
+  if (isNaN(num) || num > pages.length || num == 0) {
+    return;
+  }
+  select("#page" + currentpage).removeClass("visible");
+  select("#menu" + currentpage).removeClass("active");
+  currentpage = num;
+  select("#page" + currentpage).addClass("visible");
+  select("#menu" + currentpage).addClass("active");
 }
 
-function keyPressed(){
-  shiftpage(key)
+function keyPressed() {
+  shiftpage(key);
 }
